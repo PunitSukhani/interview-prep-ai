85f1c44112d5efa02e49cec06401b0bf67f52e4c
Punit Sukhani <sukhanipunit@gmail.com>
2025-06-26 00:34:48 +0530

    Initial commit

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div className="bg-green-600 text-white text-center p-8 text-3xl rounded-xl mt-10">
+      ✅ Tailwind is working perfectly!
+    </div>
+  );
+}
+
+
+export default App
